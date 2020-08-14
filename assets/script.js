23179d241176528df0f747da4192c8ebaa5f2468
Aaron Stoiano <aaronstoiano@gmail.com>
2020-08-14 10:55:23 -0400

    added ajax to the JS

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,10 @@
 var apiKey = "NmXoAMS92B8gPSPWNVqG1ANjCYyFTQOP";
 var searchTerm = "";
 var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?api-key=" + apiKey + "&q=" + searchTerm;
+
+$.ajax({
+    url: queryURL,
+    method: "GET"
+}).then(function (response) {
+
+})
