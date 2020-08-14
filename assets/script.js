7530832106623c664533901941da33d2662635fe
SoloJette <solemansay@gmail.com>
2020-08-14 11:15:04 -0400

    added id tag

--- assets/script.js
+++ assets/script.js
@@ -1,48 +1,5 @@
 //Our API Key
 var apiKey = "NmXoAMS92B8gPSPWNVqG1ANjCYyFTQOP";
-<<<<<<< HEAD
-var searchTerm = "";
-var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?api-key=" + apiKey + "&q=" + searchTerm;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-{/* <div id = "searchContent"> */}
-{/* <blockquote class="blockquote mb-0">
-<p> </p>
-<footer class="blockquote-footer">year <cite title="Source Title" id = "year">eg. 1996</cite></footer>
-</blockquote> */}
-{/* </div> */}
-=======
 //Grabs the search term entered and stores in a variable
 var searchTermEl = $("#searchTerm").val();
 //Our API link stored into a variable
@@ -59,4 +16,25 @@ $.ajax({
     console.log(response);
 })
 });
->>>>>>> 3534b6965b0ce59940395870b2fd675c2793b444
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+{/* <div id= "searchContent">
+<!-- <blockquote class="blockquote mb-0">
+<p> </p>
+<footer class="blockquote-footer">year <cite title="Source Title" id = "year">eg. 1996</cite></footer>
+</blockquote> -->
+</div> */}
