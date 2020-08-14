3534b6965b0ce59940395870b2fd675c2793b444
Jordan Triplett <JordanT111596@gmail.com>
2020-08-14 11:07:51 -0400

    added comments and console logs response from ajax

--- assets/script.js
+++ assets/script.js
@@ -1,10 +1,18 @@
+//Our API Key
 var apiKey = "NmXoAMS92B8gPSPWNVqG1ANjCYyFTQOP";
-var searchTermEl = $("#searchTerm");
-var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?api-key=" + apiKey + "&q=" + searchTerm;
+//Grabs the search term entered and stores in a variable
+var searchTermEl = $("#searchTerm").val();
+//Our API link stored into a variable
+var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?api-key=" + apiKey + "&q=" + searchTermEl;
+
+//When the search button is clicked
+$("#Search").on("click", function (event){
+    event.preventDefault();
 
 $.ajax({
     url: queryURL,
     method: "GET"
 }).then(function (response) {
-
+    console.log(response);
 })
+});
