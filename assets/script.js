f520d7bea8d8546985efd715a0541edd839f0db8
SoloJette <solemansay@gmail.com>
2020-08-14 11:08:38 -0400

    added divs and id's completed article card area

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,42 @@
 var apiKey = "NmXoAMS92B8gPSPWNVqG1ANjCYyFTQOP";
 var searchTerm = "";
 var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?api-key=" + apiKey + "&q=" + searchTerm;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+{/* <div id = "searchContent"> */}
+{/* <blockquote class="blockquote mb-0">
+<p> </p>
+<footer class="blockquote-footer">year <cite title="Source Title" id = "year">eg. 1996</cite></footer>
+</blockquote> */}
+{/* </div> */}
